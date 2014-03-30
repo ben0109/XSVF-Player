479f565d4428ff499a0d083e08074c8f2ceabe7e
Tormod Volden <debian.tormod@gmail.com>
2014-03-30 01:59:27 +0100

    Avoid global fd_serial and xsvf_file variables
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- client/client.c
+++ client/client.c
@@ -7,11 +7,10 @@
 #include <termios.h>
 #include "../common/xsvf.h"
 
-int serial_fd;
-FILE *xsvf_file;
-
-int setup_stream(void)
+int setup_stream(const char *devname)
 {
+	int serial_fd;
+
 	struct termios tio;
         memset(&tio,0,sizeof(tio));
         tio.c_iflag=0;
@@ -23,16 +22,14 @@ int setup_stream(void)
         cfsetospeed(&tio,B57600);	// bauds
 	cfsetispeed(&tio,B57600);
 
-	serial_fd = open("/dev/ttyUSB1", O_RDWR);
-	if (serial_fd==0) {
-		return 1;
-	}
+	serial_fd = open(devname, O_RDWR);
 
-	tcsetattr(serial_fd,TCSANOW,&tio);
-	return 0;
+	if (serial_fd >= 0)
+		tcsetattr(serial_fd, TCSANOW, &tio);
+	return serial_fd;
 }
 
-void print_line()
+void print_line(int serial_fd)
 {
 	char c;
 	do {
@@ -42,7 +39,7 @@ void print_line()
 	} while (c!='\n');
 }
 
-int command_plus()
+int command_plus(int serial_fd, FILE *xsvf_file)
 {
 	static unsigned int total = 0;
 	char buffer[0x100];
@@ -69,7 +66,7 @@ int command_plus()
 	return 0;
 }
 
-int data_ready()
+int data_ready(int serial_fd)
 {
 	size_t nbytes;
         if ( ioctl(serial_fd, FIONREAD, (char*)&nbytes) < 0 )  {
@@ -80,22 +77,26 @@ int data_ready()
 
 }
 
-int process_command(char c)
+int process_command(char c, int serial_fd)
 {
 	switch (c) {
 	case '+': return 1;
 	case '-': printf("process failed\n"); exit(1);
-	case 'd': printf("DEBUG  : "); print_line(); break;
-	case 'i': printf("INFO   : "); print_line(); break;
-	case 'w': printf("WARNING: "); print_line(); break;
-	case 'e': printf("ERROR  : "); print_line(); break;
+	case 'd': printf("DEBUG  : "); print_line(serial_fd); break;
+	case 'i': printf("INFO   : "); print_line(serial_fd); break;
+	case 'w': printf("WARNING: "); print_line(serial_fd); break;
+	case 'e': printf("ERROR  : "); print_line(serial_fd); break;
 	}
 	return 0;
 }
 
 int main()
 {
-	if (setup_stream()) {
+	int serial_fd;
+	FILE *xsvf_file;
+
+	serial_fd = setup_stream("/dev(ttyUSB1");
+	if (serial_fd < 0) {
 		exit(1);
 	}
 
@@ -104,9 +105,9 @@ int main()
 	while (1) {
 		int n;
 		char c;
-		while (data_ready()>0) {
+		while (data_ready(serial_fd) > 0) {
 			read(serial_fd, &c, 1);
-			if (process_command(c)>0) {
+			if (process_command(c, serial_fd) > 0) {
 				goto sync_ok;
 			}
 		}
@@ -121,12 +122,12 @@ sync_ok:
 
 	while (1) {
 		char c;
-		if (command_plus()) {
+		if (command_plus(serial_fd, xsvf_file)) {
 			break;
 		}
 		do {
 			read(serial_fd, &c, 1);
-		} while (process_command(c)<=0);
+		} while (process_command(c, serial_fd) <= 0);
 	}
 	return 0;
 }
